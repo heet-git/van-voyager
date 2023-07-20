743f9e230f8a8336baf051a561f44f5669e6b182
heet-git <58705043+heet-git@users.noreply.github.com>
2023-07-20 19:48:14 -0400

    Fix styling for tablet view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,7 @@ function Home(){
         <div className="home-container">
             <div className="home-intro">
                 <h1 className="intro-title">You got the travel plans, we got the travel vans.</h1>
-                <p className="intro-detail">Add adventure to your life by joining the</p> 
-                <p className="intro-detail"> <span className="hash-tag">#vanvoyager</span> movement.</p>
+                <p className="intro-detail">Add adventure to your life by joining the <span className="hash-tag">#vanvoyager</span> movement.</p>
                 <p className="intro-detail">Rent the perfect van to make your perfect road trip.</p>
                 <Link to="vans">Find your van</Link>
             </div>
