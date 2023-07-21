32ce31c8daa6656acb6d93fdf196c0c8c875fac0
heet-git <58705043+heet-git@users.noreply.github.com>
2023-07-21 19:19:47 -0400

    creating hamburger menu

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -10,10 +10,30 @@ return (
             <NavLink to ="/"className="site-name">VanVoyager</NavLink>
             </div>
             <nav className="navbar">
-                <NavLink to="/" className={({ isActive }) =>isActive ? "active" : ""}>Home</NavLink>
-                <NavLink to="vans" className={({ isActive }) =>isActive ? "active" : ""}>Vans</NavLink>
-                <NavLink to="host" className={({ isActive }) =>isActive ? "active" : ""}>Host</NavLink>
-                <NavLink to="about" className={({ isActive }) =>isActive ? "active" : ""}>About</NavLink>
+                <div className="nav-pages">
+                    <ul className="nav-menu">
+                        <li className="nav-item">
+                            <NavLink to="/" className={({ isActive }) =>isActive ? "active" : ""}>
+                            Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="vans" className={({ isActive }) =>isActive ? "active" : ""}>
+                            Vans
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="host" className={({ isActive }) =>isActive ? "active" : ""}>
+                            Host
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="about" className={({ isActive }) =>isActive ? "active" : ""}>
+                            About
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
                 <Link to="login" className="login-link">
                     <img src={profileImg} alt="profile" className="profileImg"/>
                 </Link>
